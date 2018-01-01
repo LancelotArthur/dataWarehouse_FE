3bbbbc47303eb8c2292ac3b56847ac43b387ba4a
Lancelot <1027636118@qq.com>
2018-01-01 19:23:45 +0800

    modify profile page

--- src/components/pages/profile.vue
+++ src/components/pages/profile.vue
@@ -35,16 +35,22 @@
         </div>
       </el-col>
       <el-col id="aside" :span="6" style="padding: 0">
-        <el-row style="background: #d9d9d9;border-radius: 5px;padding: 10px 5px 20px 5px">
-          <el-col :span="12" style="padding: 0">
-            <img src="../../assets/logo.png" class="image">
-          </el-col>
-          <el-col :span="12" style="padding: 0">
-            <h5 style="margin: 0">常居：<a href="#">上海</a> </h5>
-            <h5 style="margin: 0">168033009</h5>
-            <h5 style="margin: 0">{{joinTime}}加入</h5>
-          </el-col>
-        </el-row>
+        <el-col style="background: #d0e0e3;border-radius: 5px;padding: 10px 10px 20px 10px">
+          <el-row>
+            <el-col :span="12" style="padding: 0">
+              <img src="../../assets/logo.png" class="image">
+            </el-col>
+            <el-col :span="12" style="padding: 0">
+              <h5 style="margin: 0">常居：<a href="#">上海</a> </h5>
+              <h5 style="margin: 0">168033009</h5>
+              <h5 style="margin: 0">{{joinTime}}加入</h5>
+            </el-col>
+          </el-row>
+          <hr color="#cccccc" style="margin: 5px 10px 5px 10px"/>
+          <el-row style="text-align: left">
+            <h5 style="margin-top: 10px">(<a href="#">添加自我介绍</a>)</h5>
+          </el-row>
+        </el-col>
       </el-col>
     </el-row>
   </div>
