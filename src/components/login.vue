9ea28d9a5cb8e82f898afcb5cd08a144faa849ab
Lancelot <1027636118@qq.com>
2018-01-03 02:15:01 +0800

    add axios package realize login function

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,48 +1,51 @@
 <template>
-    <div style="margin: 60px">
-        <div class="login-wrap" v-show="showLogin">
-          <h3>登录</h3>
-          <div>
-            <el-input class="inputBox"
-              placeholder="请输入用户名"
-              v-model="username"
-              clearable="true">
-            </el-input>
-          </div>
-          <div>
-            <el-input class="inputBox"
-              placeholder="请输入密码"
-              v-model="password"
-              clearable="true">
-            </el-input>
-          </div>
-          <el-button type="primary" @click="login">登录</el-button><br>
-          <el-button type="text" @click="ToRegister">还没有账号？马上注册</el-button>
-        </div>
-
-        <div class="register-wrap" v-show="showRegister">
-            <h3>注册</h3>
-            <div>
-              <el-input class="inputBox"
-                placeholder="请输入用户名"
-                v-model="newUsername"
-                clearable>
-              </el-input>
-            </div>
-            <div>
-              <el-input class="inputBox"
-                placeholder="请输入密码"
-                v-model="newPassword"
-                clearable>
-              </el-input>
-            </div>
-            <el-button type="primary" @click="register">注册</el-button><br>
-            <el-button type="text" @click="ToLogin">已有账号？马上登录</el-button>
-        </div>
+  <div style="margin: 60px">
+    <div class="login-wrap" v-show="showLogin">
+      <h3>登录</h3>
+      <div>
+        <el-input class="inputBox"
+          placeholder="请输入用户名"
+          v-model="info.userName"
+          clearable="true">
+        </el-input>
+      </div>
+      <div>
+        <el-input class="inputBox"
+          placeholder="请输入密码"
+          v-model="info.userPassword"
+          clearable="true">
+        </el-input>
+      </div>
+      <el-button type="primary" @click="login">登录</el-button><br>
+      <el-button type="text" @click="ToRegister">还没有账号？马上注册</el-button>
     </div>
+
+    <div class="register-wrap" v-show="showRegister">
+      <h3>注册</h3>
+      <div>
+        <el-input class="inputBox"
+          placeholder="请输入用户名"
+          v-model="newUsername"
+          clearable>
+        </el-input>
+      </div>
+      <div>
+        <el-input class="inputBox"
+          placeholder="请输入密码"
+          v-model="newPassword"
+          clearable>
+        </el-input>
+      </div>
+      <el-button type="primary" @click="register">注册</el-button><br>
+      <el-button type="text" @click="ToLogin">已有账号？马上登录</el-button>
+    </div>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+import qs from 'qs'
+
 export default {
   data () {
     return {
@@ -50,59 +53,56 @@ export default {
       showRegister: false,
       showTishi: false,
       tishi: '',
-      username: '',
-      password: '',
       newUsername: '',
-      newPassword: ''
+      newPassword: '',
+      info: {
+        userName: '',
+        userPassword: ''
+      }
     }
   },
   methods: {
     login: function () {
-      // this.$http.post(
-      //   'localhost:8888/login',
-      //   {
-      //     userName: 'gst3',
-      //     userPassword: 'pppp'
-      //   },
-      //   {
-      //     'headers': {
-      //       'Content-Type': 'application/x-www-form-urlencoded'
-      //     }
-      //   }
-      // ).then(function (response) {
-      //   alert(response.data)
-      // }, function (response) {
-      //   alert(response.headers)
-      //   alert('something wrong')
-      // })
-      this.$http({
-        url: 'localhost:8888/login',
-        method: 'POST',
-        data: {
-          userName: 'gst3',
-          userPassword: 'pppp'
-        },
+      axios.post('http://localhost:8888/login', qs.stringify(this.info), {
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded'
         }
-      }).then(function (response) {
-        alert(response.data)
-      }, function (response) {
-        alert(response.headers)
-        alert('something wrong')
+      }).then(response => {
+        if (response.data.status === '200') {
+          // sessionStorage.setItem('token', response.data.result.token)
+          this.$confirm('是否要保存密码', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'info'
+          }).then(() => {
+            this.$router.push('/homePage')
+            // localStorage.setItem('userName', this.info.userName)
+            // localStorage.setItem('userPassword', this.info.userPassword)
+            this.$message({
+              type: 'success',
+              message: '保存成功!'
+            })
+          }).catch(() => {
+            this.$router.push('/homePage')
+            this.$message({
+              type: 'success',
+              message: '登录成功'
+            })
+          })
+        } else if (response.data.status === '401') {
+          this.$message({
+            type: 'success',
+            message: response.data.result
+          })
+        }
+      }, response => {
+        this.$message({
+          message: '服务器错误',
+          type: 'error'
+        })
       })
-      // this.$router.push('/homePage')
     },
-    register: function () {
-      this.$http.get(
-        'localhost:8888/movie/22266012'
-      ).then(function (response) {
-        alert(response.data)
-      }, function (response) {
-        alert(response.headers)
-        alert('something wrong')
-      })
-    },
+    register: function () { },
     ToRegister: function () {
       this.showLogin = false
       this.showRegister = true
@@ -116,16 +116,16 @@ export default {
 </script>
 
 <style>
-  .inputBox {
-    margin: 10px;
-  }
-  .login-wrap {
-    text-align: center;
-    width: 25%;
-    margin: 0 auto;
-  }
-  .register-wrap {
-    width: 25%;
-    margin: 0 auto;
-  }
+.inputBox {
+  margin: 10px;
+}
+.login-wrap {
+  text-align: center;
+  width: 25%;
+  margin: 0 auto;
+}
+.register-wrap {
+  width: 25%;
+  margin: 0 auto;
+}
 </style>
